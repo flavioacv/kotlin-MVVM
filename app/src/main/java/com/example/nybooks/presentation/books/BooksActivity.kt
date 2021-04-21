ca3adba8e622f5b1c8750c88e4d55abf5d19ea11
Flavio Augusto <flavioacv.soft@gmail.com>
2021-04-21 11:57:03 -0400

    Adicionando os componentes Toolbar e RecyclerView no projeto

--- app/src/main/java/com/example/nybooks/presentation/books/BooksActivity.kt
+++ app/src/main/java/com/example/nybooks/presentation/books/BooksActivity.kt
@@ -1,12 +1,20 @@
 package com.example.nybooks.presentation.books
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.nybooks.R
+import kotlinx.android.synthetic.main.activity_books.*
+
 
 class BooksActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_books)
+
+
+        toolbarMain.title = "Books"
+        setSupportActionBar(toolbarMain)
     }
 }
