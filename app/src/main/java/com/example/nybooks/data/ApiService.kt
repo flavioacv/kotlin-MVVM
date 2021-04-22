2e9891ab1c709c819ac1ae6d823fbfab74fcbd62
Flavio Augusto <flavioacv.soft@gmail.com>
2021-04-22 08:21:21 -0400

    Criação da configurações do Retrofit e Moshi

--- app/src/main/java/com/example/nybooks/data/ApiService.kt
+++ app/src/main/java/com/example/nybooks/data/ApiService.kt
@@ -1,4 +1,16 @@
 package com.example.nybooks.data
 
-class ApiService {
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+
+object ApiService {
+    private fun initRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.nytimes.com/svc/books/v3/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+
+    val service = initRetrofit().create(NYTServices::class.java)
 }
--- app/src/main/java/com/example/nybooks/data/NYTServices.kt
+++ app/src/main/java/com/example/nybooks/data/NYTServices.kt
@@ -1,4 +1,12 @@
 package com.example.nybooks.data
 
+import retrofit2.Call
+import retrofit2.http.GET
+
+
+
+
 interface NYTServices {
+    @GET("lists.json")
+    fun listRepos(): Call<List<>>
 }
