b7430010a382268aa8ccfe7728bc6a5fe456c70e
Flavio Augusto <flavioacv.soft@gmail.com>
2021-04-24 11:17:35 -0400

    Aplicando estilos e componentizando

---
+++ app/src/main/java/com/example/nybooks/presentation/base/BaseActivity.kt
@@ -0,0 +1,13 @@
+package com.example.nybooks.presentation.base
+
+
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity : AppCompatActivity() {
+    protected fun setupToolbar(toolbar: androidx.appcompat.widget.Toolbar, titleIdRes: Int) {
+        toolbar.title = getString(titleIdRes)
+        setSupportActionBar(toolbar)
+    }
+
+
+}
--- app/src/main/java/com/example/nybooks/presentation/books/BooksActivity.kt
+++ app/src/main/java/com/example/nybooks/presentation/books/BooksActivity.kt
@@ -1,27 +1,26 @@
 package com.example.nybooks.presentation.books
 
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.nybooks.R
-import com.example.nybooks.data.model.Book
+import com.example.nybooks.presentation.base.BaseActivity
 import com.example.nybooks.presentation.details.BookDetailsActivity
 import kotlinx.android.synthetic.main.activity_books.*
+import kotlinx.android.synthetic.main.include_toolbar.*
 
 
-class BooksActivity : AppCompatActivity() {
+class BooksActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_books)
 
+        setupToolbar(toolbarMain, R.string.books_title)
 
-        toolbarMain.title = getString(R.string.books_title)
-        setSupportActionBar(toolbarMain)
 
         val viewModel: BooksViewModel = ViewModelProviders.of(this).get(BooksViewModel::class.java)
 
