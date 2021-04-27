cb4fd7840386fc1d3712684361b35d294914b9fb
Flavio Augusto <flavioacv.soft@gmail.com>
2021-04-27 09:02:54 -0400

    Ajuste de Arquitetura para Teste

--- app/src/main/java/com/example/nybooks/presentation/books/BooksActivity.kt
+++ app/src/main/java/com/example/nybooks/presentation/books/BooksActivity.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.nybooks.R
+import com.example.nybooks.data.repository.BooksApiDataSource
 import com.example.nybooks.presentation.base.BaseActivity
 import com.example.nybooks.presentation.details.BookDetailsActivity
 import kotlinx.android.synthetic.main.activity_books.*
@@ -21,8 +22,7 @@ class BooksActivity : BaseActivity() {
 
         setupToolbar(toolbarMain, R.string.books_title)
 
-
-        val viewModel: BooksViewModel = ViewModelProviders.of(this).get(BooksViewModel::class.java)
+        val viewModel: BooksViewModel = BooksViewModel.ViewModelFactory(BooksApiDataSource()).create(BooksViewModel::class.java)
 
         viewModel.booksLiveData.observe(this, Observer {
             it?.let { books ->
